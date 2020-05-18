83e5ebf1d1d8c7a597da654caac4ae532920748a
Jack Frigaard <jack.frigaard@gmail.com>
2020-05-18 19:10:11 +0930

    Removed old comment about next_transition_instant

--- src/discovery.jl
+++ src/discovery.jl
@@ -127,29 +127,15 @@ end
 
 
 """
-    next_transition_instant(zdt::ZonedDateTime) -> Union{ZonedDateTime, Nothing}
-    next_transition_instant(tz::TimeZone=localzone()) -> Union{ZonedDateTime, Nothing}
+    next_transition_instant(zdt::ZonedDateTime) -> Union{Tuple{DateTime, FixedTimeZone, FixedTimeZone}, Nothing}
+    next_transition_instant(tz::TimeZone=localzone()) -> Union{Tuple{DateTime, FixedTimeZone, FixedTimeZone}, Nothing}
 
 Determine the next instant at which a time zone transition occurs (typically
 due to daylight-savings time). If no there exists no future transition then `nothing` will
 be returned.
 
-Note that the provided `ZonedDateTime` isn't normally constructable:
-
-```julia
-julia> instant = next_transition_instant(ZonedDateTime(2018, 3, 1, tz"Europe/London"))
-2018-03-25T01:00:00+00:00
-
-julia> instant - Millisecond(1)  # Instant prior to the change
-2018-03-25T00:59:59.999+00:00
-
-julia> instant - Millisecond(0)  # Instant after the change
-2018-03-25T02:00:00+01:00
-
-julia> ZonedDateTime(2018, 3, 25, 1, tz"Europe/London")  # Cannot normally construct the `instant`
-ERROR: NonExistentTimeError: Local DateTime 2018-03-25T01:00:00 does not exist within Europe/London
-...
-```
+Returns the naive instant of the transition, and the fixed time zones active before and after
+the transition.
 """
 next_transition_instant
 
@@ -166,12 +152,6 @@ function next_transition_instant(zdt::ZonedDateTime{VariableTimeZone})
 
     index <= length(tz.transitions) || return nothing
 
-    # Use the UTC datetime of the transition and the offset information prior to the
-    # transition to create a `ZonedDateTime` which cannot be constructed with the high-level
-    # constructors. The instant constructed is equivalent to the first instant after the
-    # transition but visually appears to be before the transition. For example in a
-    # transition where the clock changes from 01:59 → 03:00 we would return 02:00 where
-    # the UTC datetime of 02:00 == 03:00.
     utc_datetime = tz.transitions[index].utc_datetime
     from_zone = tz.transitions[index - 1].zone
     to_zone = tz.transitions[index].zone
